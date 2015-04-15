952ec540e37f74f3b3628940e41fba557bf8c9cb
Jordan <jbhansen84@gmail.com>
2015-04-15 17:00:42 -0600

    With firebase

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,18 @@
 (function(){
 
-	var homeController = function ($scope, $http){
-    }
-    homeController.$inject = ['$scope', '$http'];
+	var homeController = function ($scope, $http, $firebaseArray){
+        var ref = new Firebase("https://ang-boot-fire.firebaseio.com/");
+        $scope.chats = $firebaseArray(ref); 
+        console.log($scope.chats);
+        
+        $scope.newChat = function(){
+            $scope.chitter.createDate = new Date().toGMTString();
+            $scope.chats.$add($scope.chitter);
+            $scope.chitter.text = "";
+            
+        };
+    };
+    homeController.$inject = ['$scope', '$http', '$firebaseArray'];
     app.controller('homeController', homeController); 
     
 
